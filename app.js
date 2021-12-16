8d6412e7964c06f389f173b57107685930c51d15
Pete Seeling <peteseeling@gmail.com>
2021-12-16 13:32:51 -0500

    Html Done

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 // import functions and grab DOM elements
 
+const form = document.getElementById('form');
+const questionEl = document.getElementById('question');
+const optionATitleEl = document.getElementById('option-a-title');
+const optionAVotesEl = document.getElementById('option-a-votes');
+
+const optionBTitleEl = document.getElementById('option-b-title');
+const optionBVotesEl = document.getElementById('option-b-votes');
+const optionAAddButton = document.getElementById('option-a-add');
+const optionBAddButton = document.getElementById('option-b-add');
+const closePollButton = document.getElementById('close-poll');
+
 // let state
 
 // set event listeners 
